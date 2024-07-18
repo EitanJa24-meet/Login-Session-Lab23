30dd94c30bcdd3edfd0dea35b358d9054b3713ea
EitanJa24-meet <eitanjacobs7@gmail.com>
2024-07-18 16:41:44 +0300

    quote boared

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -1,32 +1,45 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask import session as login_session
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key'
+app = Flask(__name__,
+template_folder='templates',
+static_folder='static')
 
-@app.route('/', ) # What methods are needed?
+app.config['SECRET_KEY'] = 'Your_secret_string'
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+	try:
+		if request.method == "POST":
+			Name = request.form["name"]
+			Quote = request.form["quote"]
+			Age = request.form["age"]
+			login_session['name'] = Name
+			login_session['quote']  = Quote
+			login_session['age'] = Age
+			print(login_session)
+			return redirect(url_for('thanks'))
+		else:	
+			return render_template("home.html")
+	except:
+		return redirect(url_for('error'))
+
 	
-	return render_template('home.html')
-
-
-@app.route('/error')
+@app.route('/error', methods=['GET', 'POST'])
 def error():
-
-	return render_template('error.html')
-
-
-@app.route('/display')
-def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	return render_template("error.html")
 
 
-@app.route('/thanks')
+@app.route('/thanks', methods=['GET', 'POST'])
 def thanks():
+	return render_template("thanks.html")
 
-	return render_template('thanks.html')
+@app.route('/display', methods=['GET', 'POST'])
+def display():
+	return render_template("display.html")
+
 
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
